78cfb5afe9677ec224d2e56ad74e7551bc28911d
morauxpaul-jpg <moraux.paul@gmail.com>
2026-01-24 01:22:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import gspread
 
 # --- CONFIGURATION MOBILE ---
-st.set_page_config(page_title="App Muscu", layout="centered", page_icon="💪")
+st.set_page_config(page_title="App Muscu", layout="centered", page_icon="logo.png")
 
 # --- DESIGN MODERNE ---
 st.markdown("""
@@ -217,3 +217,4 @@ with tab3:
             with st.expander("Voir tout l'historique"):
                 df_clean = df_exo[["Semaine", "Série", "Reps", "Poids", "Remarque"]].sort_values(by=["Semaine", "Série"], ascending=[False, True])
                 st.dataframe(df_clean, use_container_width=True, hide_index=True)
+
